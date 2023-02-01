01b6fbc2f9414c8bbccc152c7dd8733eaa3ea616
Alexey Kabaiev <kabaievalex@gmail.com>
2023-02-01 16:40:01 +0200

    task-3

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,38 @@
+//Используй массив объектов images для создания элементов <img> вложенных в <li>.
+//Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+//Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+//Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+// const createGalleryItem = ({ url, alt }) =>
+//   `<li><img src="${url}" alt="${alt}" width = 300 height = 200></li>`;
+// const galleryMarkup = images.reduce(
+//   (acc, item) => acc + createGalleryItem(item),
+//   ""
+// );
+
+const galleryMarkup = images.map(({ url, alt }) => {
+  const image = `<li><img src="${url}" alt="${alt}" width = 300 height = 200></li>`;
+  return image;
+});
+console.log(galleryMarkup);
+const galleryList = document.querySelector(".gallery");
+galleryList.insertAdjacentHTML("afterbegin", galleryMarkup.join(" "));
+galleryList.setAttribute(
+  "style",
+  "list-style-type:none; display:flex; gap:30px"
+);
