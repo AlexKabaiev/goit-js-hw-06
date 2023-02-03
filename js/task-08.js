ab8621b85a6ca582b4c77c1688a609e7bde2618a
Alexey Kabaiev <kabaievalex@gmail.com>
2023-02-03 16:51:49 +0200

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,54 @@
 
+//Напиши скрипт управления формой логина.
+
+{/* <form class="login-form">
+  <label>
+    Email
+    <input type="email" name="email" />
+  </label>
+  <label>
+    Password
+    <input type="password" name="password" />
+  </label>
+  <button type="submit">Login</button>
+</form> */}
+
+//Обработка отправки формы form.login-form должна быть по событию submit.
+//При отправке формы страница не должна перезагружаться.
+//Если в форме есть незаполненные поля, выводи alert с предупреждением о том,
+//что все поля должны быть заполнены.
+//Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект,
+//где имя поля будет именем свойства, а значение поля - значением свойства.
+//Для доступа к элементам формы используй свойство elements.
+//Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+
+var render = document.querySelector('[data-action="render"]');
+var destroy = document.querySelector('[data-action="destroy"]');
+var boxes = document.getElementById("boxes");
+render.addEventListener("click", getAmount);
+destroy.addEventListener("click", destroyBoxes);
+
+function getAmount() {
+  var amount = +document.querySelector("#controls input").value;
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  var basicSize = 30;
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < amount; i++) {
+    var size = basicSize + i * 10;
+    var div = document.createElement("div");
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
+    fragment.appendChild(div);
+  }
+  boxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+function random() {
+  return Math.floor(Math.random() * 256);
+}
